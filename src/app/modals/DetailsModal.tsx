9b27bbb2d2da21284a21d5430359f21d14889918
fjanusz <72928966+fjanusz@users.noreply.github.com>
2020-11-06 13:14:07 +0100

    INTERVIEW-6 Product details modal (#6)
    * Product details modal
    
    * Delete debug.log

---
+++ src/app/modals/DetailsModal.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalBody,
+    ModalCloseButton,
+    Image,
+    Text,
+} from "@chakra-ui/core";
+
+interface ToggleProps {
+    isOpen: boolean,
+    onClose: () => void,
+    state: {
+        image: string,
+        description: string,
+        name: string,
+    },
+}
+
+const DetailsModal = (Props: ToggleProps) => {
+    return (
+        <Modal size="xl" isOpen={Props.isOpen} onClose={Props.onClose}>
+            <ModalOverlay />
+            <ModalContent rounded="md">
+                <ModalHeader p={0}>
+                    <Image w="100%" src={Props.state.image} roundedTop="md" />
+                </ModalHeader>
+                <ModalCloseButton border="none" bg="transparent" _focus={{ outline: "none" }} />
+                <ModalBody pt={6} pb={12}>
+                    <Text color="standardBlack" pb={3}>{Props.state.name}</Text>
+                    <Text color="darkGray" fontSize="sm">{Props.state.description}</Text>
+                </ModalBody>
+            </ModalContent>
+        </Modal>
+    );
+}
+
+export default DetailsModal;
